d6fced0b8ed0a1789f5256a27cf5135f90825034
Jwilson1172 <jw59615@gmail.com>
2020-07-20 18:22:04 -0400

    working on the CPU functionallity

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,34 +2,95 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # for speed I want to implement ram as a hashtable, because its
+        # accurate to how ram works
+        # this implementation is un-sized which means that it can be exploited
+        # during execution I want to make sure that I check the size of this
+        self.ram = {}
+        # init a register
+        self.reg = {}
+        # init a program counter
+        self.pc = 0
+
+        # not sure if I need anything else here
 
     def load(self):
         """Load a program into memory."""
-
-        address = 0
+        # check if there is an alt adress to load into
+        # (great to inject code into the runtime from pdb)
+        if ALT_ADR != None:
+            address = ALT_ADR
+        # if there is not an injection address then just mount the program from
+        # the first address, this will rewrite the ram for any program that
+        # is running
+        else:
+            address = 0
 
         # For now, we've just hardcoded a program:
-
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, addy):
+        """Returns the content of a memory address
+        Arguments:
+        ----------
+        `addy` {str or int} - the address to use to fetch data from the ram
+        Returns:
+        ----------
+        data {Object} - the data that is stored in the ram at `addy`"""
+
+        if isinstance(addy, list):
+            raise ValueError("List is not an expected input to this function\n\
+                please use a string or int")
+        else:
+            return self.ram[addy]
+
+        # this should be unreachable code
+        # but i still want to make my linter happy
+        return None
+
+    def ram_write(self, addy, d):
+        """A function that takes some data then adds it at address `addy`
+        Only compleats if the len(d) < len(free_ram) and
+        ram[d.index.min() : d.index.max()] (has to fit)
+        """
+
+        # check if there is something already in that address
+        if self.ram[addy] is not None:
+            print(
+                "the memory at the address of {addy} is already taken by another program"
+            )
+            print("Content of RAM[{addy}]:\n", self.ram[addy])
+            return False
+
+        # check to make sure that the ram can hold the data that is being loaded
+        # eg os.sizeof(d) > 100Tb is obviously not going into ram,
+        # this protects against overflows
+        if len(self.ram) < len(d):
+            print(
+                "Overflow Killswitch got triggered, please use a smaller data\n\
+                source or use a buffered reader if you are streaming from a file"
+            )
+            return False
+
+        # should be unreachable code
+        return False
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -46,14 +107,15 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |" % (
+                self.pc,
+                #self.fl,
+                #self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2)),
+            end='')
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
