f8ba46491293d7ba016be60abb24a049015cf2a8
Jwilson1172 <jw59615@gmail.com>
2020-07-24 04:14:47 -0400

    adding instructions and refactoring run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -32,34 +32,29 @@ class CPU:
     # by default I want to make RAM 256 bytes this is the default for the ls-8
     # I also wanted to leave the option open for increasing the ram_size to
     # something larger to hold larger programs/stacks
-    def __init__(self, DBG=False, ram_size=256, cores=1):
+    def __init__(self, DBG=False, ram_size=256):
         """Construct a new CPU."""
-        # the location of the currently executing program
-        if cores > 1:
-            # this is added because at some point i would like to implement
-            # a multiprocessing solution to this class
-            raise NotImplementedError("this feature is not implemented please\
-                please just use one core for now")
-
         # set to NOP by default
-        self.pc = 0b00000000
+        self.pc = 0
+
+        # global toggle settings
+        self.running = False
 
         # the IR register is just going to hold a copy of the PC reg
-        self.ir = self.pc
+        self.ir = 0
 
         # init the execution interupt registry
         self.ie = 0
 
-        # stack pointer is going to be init to 0 but will change later in
-        # self.run()
-        self.sp = 4
+        # stack pointer set to the 7th registry
+        self.sp = 7
 
-        # general pourpose registry for holding the arguments for alu
-        # operations
-        self.reg = [] * 8
+        # general pourpose registry for holding the arguments for the operations
+        self.reg = [0] * 8
 
         # flag register
         self.fl = 0
+
         # initalize the hash table that will be acting as ram
         self.RAM = {k: None for k in range(ram_size)}
 
@@ -82,16 +77,33 @@ class CPU:
     LD = 0b10000011
     ST = 0b10000100
     PRN = 0b01000111
+    PRA = 0b01001000
+
+    # alu operations
+    ADD = 0b10100000
+    MLP = 0b10100010
+    DIV = 0b10100011
+    DEC = 0b01100110
+    CMP = 0b10100111
+    AND = 0b10101000
+    NOT = 0b01101001
+    SUB = 0b10100001
+    SHL = 0b10101100
+    SHR = 0b10101101
+
+    # stack
     PUSH = 0b01000101
     POP = 0b01000110
-    PRA = 0b01001000
-    CMP = 0b10100111
+
+    ###########################################################################
+    #                        Instructions Functions                           #
+    ###########################################################################
 
     def cmp(self, reg_a, reg_b) -> None:
         """This Function takes the regerister arguments and sets the flag register
         accordingly see tyhe spec for a breakdown on the flags
         """
-            # adding the compare operation
+        # adding the compare operation
         if reg_a == reg_b:
             self.fl = 0b00000001
         elif reg_a > reg_b:
@@ -107,28 +119,24 @@ class CPU:
         self.pc = address
         return None
 
-    def nop(self, reg_a, reg_b):
+    def nop(self):
         return
 
-    def hlt(self, reg_a, reg_b):
-        exit()
+    def hlt(self):
+        self.running = False
         return
 
     def ldi(self, reg_a, reg_b):
-        reg_a = int(str(reg_b), 10)
         return
+
     def aand(self, reg_a, reg_b):
-        reg_a = reg_a & reg_b
+        self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+        return
 
-    self.dispach_table = {
-        JMP:self.jmp,
-        NOP:self.nop,
-        HLT:self.hlt,
-        LDI:self.ldi,
-        AND:self.aand,
+    def prn(self, reg_a, reg_b):
+        print(self.reg[reg_a])
+        return
 
-
-    }
     def load(self, p):
         """Load a program into memory."""
         # changing this number of the binary form of the int to I can have
@@ -155,9 +163,10 @@ class CPU:
                     address += 1
                 except ValueError:
                     pass
-#######################################################
-#           ALU OPPERATIONS                           #
-#######################################################
+
+    ###########################################################################
+    #                       ALU OPPERATIONS                                   #
+    ###########################################################################
 
     def alu(self, op: int, reg_a, reg_b) -> type(None):
         """ALU for the cpu that handles a veritity of operations preformed
@@ -167,48 +176,17 @@ class CPU:
         `reg_a` : the first register to take from
         `reg_b` : the second register to act on
         `op` : a binary opperator that tells the alu what to execute
-
-        * for CMP and some of the other operators this isn't nessisarly the case
-        for instance during a LDI(op code: 0b10000010) operation the reg_b is
-        the integer value that is going to be put on the reg_a aka op argument:
-        iiiiiiii
-        Returns:
-        ------------
-        type(None)
-        (ideally this isn't going to return anything just modify the state of the internal vars)
         """
-        # AND
-        elif op == :
-            reg_a = reg_a & reg_b
-
-        # LD
-        elif op == 0b10000011:
-            # not sure what LD does going to implement later
-            pass
-
-        # ST
-        elif op == 0b10000100:
-            # again not sure what this does atm
-            pass
-        # PUSH
-        elif op == 0b01000101:
-            # this method should push a value onto a stack
-            # I think that the stack is R7 which is that stack pointer register
-            pass
-        # POP
-        elif op == 0b01000110:
-            #Same thing as push but instead its poping a value off of a stack
-            pass
-        # PRN
-        elif op == 0b01000111:
-            print(reg_a)
-        # PRA
-        elif op == 0b01001000:
-            # not sure what PRA does
-            pass
-        else:
-            self.trace()
-            raise Exception("Unsupported ALU operation")
+        self.dispatch_table = {
+            self.JMP: self.jmp,
+            self.NOP: self.nop,
+            self.HLT: self.hlt,
+            self.LDI: self.ldi,
+            self.AND: self.aand,
+            self.PRN: self.prn,
+        }
+        self.dispatch_table[op](reg_a, reg_b)
+        return None
 
     def ram_read(self, address):
         """Returns the Data that is stored in the ram at `address`
@@ -221,9 +199,9 @@ class CPU:
         """
         return self.RAM[address]
 
-#############################################################
-#           CPU DEBUGGING FUNCTIONS                         #
-#############################################################
+    ###########################################################################
+    #                 CPU DEBUGGING FUNCTIONS                                 #
+    ###########################################################################
 
     def trace(self):
         """
@@ -249,49 +227,21 @@ class CPU:
             print(i)
         return self.reg
 
+    ###########################################################################
+    #                              MAIN                                       #
+    ###########################################################################
     def run(self):
         """Starts the main execution of the program"""
-        running = True
+        self.running = True
         # a simple count to figure out how many times my cpu has cycled
         clock = 0
         while running:
-            clock += 1
-            # read next op from ram
-            op = self.ram_read(self.pc)
-            if self.DBG:
-                self.trace()
-
-            # set the IR to reflect the PC
-            self.ir = op
-            if self.DBG:
-                self.trace()
-
-            # unpack the arguments from the next two slots in ram
-            a0, a1 = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
-            if self.DBG:
-                self.trace()
-
-            # send the op and arguments to the alu
-            self.alu(op, a0, a1)
-            if self.DBG:
-                self.trace()
-
-            # going to want to increment past the arguments that were used
-            # in the last operator
-            self.pc += 3
-            if self.DBG:
-                self.trace()
-                print(f"clock_num: {clock}")
         return None
 
 
-#############################################################
-#              SUPPORT FUNCTIONS/CLASSES                    #
-#############################################################
-
 if __name__ == '__main__':
     # init the cpu in debug mode with 256 bytes of ram and 1 compute core
-    cpu = CPU(DBG=True, ram_size=256, cores=1)
+    cpu = CPU()
 
     # load a file from the local directory to run
     p = cpu.load_file("./examples/print8.ls8")
